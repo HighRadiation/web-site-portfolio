c76b3746a2d7e63dac4f070c7e110cf17d92c7fd
Buğra Öksüz <bugraoksuz6@gmail.com>
2026-04-24 23:14:37 +0300

    chore: redirect /docs in production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  ...(process.env.NODE_ENV === 'production' && {
+    redirects: async () => [
+      { source: '/docs', destination: '/', permanent: false }
+    ]
+  })
 };
 
 export default nextConfig;
